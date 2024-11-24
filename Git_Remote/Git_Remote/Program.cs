c8afb67da7bc8bd9eb61d95ad236c170eb1f1c1b
Ali <99713703+AspDotNet-Projects@users.noreply.github.com>
2024-11-24 21:18:18 +0330

    Privatre methode in Home

--- Git_Remote/Git_Remote/Program.cs
+++ Git_Remote/Git_Remote/Program.cs
@@ -15,6 +15,10 @@ namespace Git_Remote
             ///
             Object objj = 123;
             int numm = (int) objj;
+
+
+            privatemethode pr = new privatemethode(Console.ReadLine());
+            var a = pr.Name;
             Console.WriteLine("num:"+numm);
             Console.WriteLine("obg:"+objj);
             Console.ReadLine();
---
+++ Git_Remote/Git_Remote/privatemethode.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git_Remote
+{
+    public class privatemethode
+    {
+        private string name { get; set; }
+
+        public string Name
+        {
+            get { return name; }
+            set { name=value; }
+
+        }
+
+        public privatemethode(string name)
+        {
+            Name = name;
+        }
+    }
+}
