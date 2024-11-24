8435f083e3e8bac870b6fefb8a1fb871847926be
Ali <99713703+AspDotNet-Projects@users.noreply.github.com>
2024-11-24 20:53:41 +0330

    Commit In Home

---
+++ Git_Remote/Git_Remote/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Git_Remote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
